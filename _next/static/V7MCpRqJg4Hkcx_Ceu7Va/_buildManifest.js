573fdea2dd57bc3af7bc800c1543f31155e63f89
lucyach <lucyacheson54@gmail.com>
2026-01-28 22:18:57 -0800

    Updates

--- _next/static/V7MCpRqJg4Hkcx_Ceu7Va/_buildManifest.js
+++ _next/static/V7MCpRqJg4Hkcx_Ceu7Va/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/personal-website//_next/:path+",
-        "destination": "/personal-website/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
