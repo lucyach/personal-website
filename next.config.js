9a435a77eb76a04cc8f9bdcc556129f918112dd9
lucyach <lucyacheson54@gmail.com>
2026-01-28 22:23:23 -0800

    Add .nojekyll file and fix assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export', // Ensure static export is enabled
   trailingSlash: true, // Add trailing slashes for GitHub Pages compatibility
   basePath: process.env.NODE_ENV === 'production' ? '/personal-website' : '', // Conditionally set basePath
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-website/' : '', // Conditionally set assetPrefix
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-website' : '', // Remove trailing slash from assetPrefix
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
